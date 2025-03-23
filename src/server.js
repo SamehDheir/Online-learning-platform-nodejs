dadd0e1687b1c333b9e6ae60ce94bddde2cb7748
SamehDheir <dheir2000@gmail.com>
2025-03-23 11:11:18 +0200

    update Port

--- src/server.js
+++ src/server.js
@@ -40,7 +40,6 @@ const io = new Server(server, {
 });
 app.use(cors());
 
-
 // Setting Swagger
 const swaggerOptions = {
   definition: {
@@ -88,4 +87,6 @@ chatSocket(io);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
